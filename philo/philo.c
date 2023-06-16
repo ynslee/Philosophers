6c4f0feea13aee7cde138bbed136696336a728e1
chuchilini <vanesalees@gmail.com>
2023-06-16 18:53:47 +0300

    --all

--- philo/philo.c
+++ philo/philo.c
@@ -6,7 +6,7 @@
 /*   By: yoonslee <yoonslee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/15 15:17:21 by yoonslee          #+#    #+#             */
-/*   Updated: 2023/06/15 17:10:30 by yoonslee         ###   ########.fr       */
+/*   Updated: 2023/06/16 18:50:52 by yoonslee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,8 @@
 int	main(int argc, char **argv)
 {
 	if (argc != 5)
-		exit(ARGS_ERROR);
+	{
+		printf("wrong argument number"):
+	}
 	
 }
