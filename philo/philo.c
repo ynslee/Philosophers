a569e8a4c0a7ac90da990d555e69f3c8320b5d15
chuchilini <vanesalees@gmail.com>
2023-07-06 16:32:29 +0300

    --all

--- philo/philo.c
+++ philo/philo.c
@@ -6,13 +6,14 @@
 /*   By: yoonslee <yoonslee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/15 15:17:21 by yoonslee          #+#    #+#             */
-/*   Updated: 2023/06/16 19:03:33 by yoonslee         ###   ########.fr       */
+/*   Updated: 2023/07/06 16:31:02 by yoonslee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-// memset, prinft, malloc, free, write, usleep, gettimeofday, pthread_create, pthread_join,
+// memset, prinft, malloc, free, write, usleep, gettimeofday, 
+// pthread_create, pthread_join, pthread_detach,
 // pthread_mutex_init,
 // pthread_mutex_destroy, pthread_mutex_lock,
 // pthread_mutex_unlock
